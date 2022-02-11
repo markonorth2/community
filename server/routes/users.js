bd60efc0dccfb92f6bebebfeecd3b345d72bb614
Mark Chuang <markchuangth@gmail.com>
2022-02-11 16:09:33 -0500

    add user test data to homepage

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,10 +1,12 @@
 var express = require('express');
 var router = express.Router();
+
+const users = ['Daniel', 'Jaylen', 'Mark'];
  
 
 module.exports = () => {
   router.get('/', (req, res) =>{
-
+    res.json(users);
   })
   return router;
 }
