4349ba51bc2f8378aca9f147acfb85109ab0c242
Mark Chuang <markchuangth@gmail.com>
2022-02-11 16:00:52 -0500

    refactor users route

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var router = express.Router();
+ 
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+module.exports = () => {
+  router.get('/', (req, res) =>{
 
-module.exports = router;
+  })
+  return router;
+}
