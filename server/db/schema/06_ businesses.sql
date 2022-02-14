cdaefd38239dc7446b6b92c0eadf2a506821656e
Daniel Lu <daniel_lu@hotmail.com>
2022-02-13 22:01:57 -0500

    schema completed. There are 7 tables

---
+++ server/db/schema/06_ businesses.sql
@@ -0,0 +1,20 @@
+-- Drop and recreate businesses table
+
+DROP TABLE IF EXISTS businesses CASCADE;
+
+CREATE TABLE businesses (
+  id SERIAL PRIMARY KEY NOT NULL,
+  category_id INTEGER REFERENCES categories ON DELETE CASCADE,
+  name VARCHAR(255) NOT NULL,
+  city VARCHAR (255) NOT NULL,
+  province_state VARCHAR (255) NOT NULL,
+  country VARCHAR (255) NOT NULL,
+  unit_number VARCHAR (255),
+  street_address VARCHAR (255) NOT NULL,
+  postal_code VARCHAR (255) NOT NULL,
+  phone_number VARCHAR (255) NOT NULL,
+  website_url  VARCHAR (255) NOT NULL,
+  image_url VARCHAR (255) NOT NULL,
+  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  modified_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
---
+++ server/db/schema/07_ratings.sql
@@ -0,0 +1,11 @@
+-- Drop and recreate ratings table
+
+DROP TABLE IF EXISTS ratings CASCADE;
+
+CREATE TABLE ratings (
+  id SERIAL PRIMARY KEY NOT NULL,
+  business_id INTEGER REFERENCES businesses ON DELETE CASCADE,
+  report_id INTEGER REFERENCES rating ON DELETE CASCADE,
+  customer_service_rating SMALLINT NOT NULL DEFAULT 0,
+  product_rating SMALLINT NOT NULL DEFAULT 0,
+);
