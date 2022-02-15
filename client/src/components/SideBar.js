1594254ffe6c084b9a1fdf946485169a22553382
Daniel Lu <daniel_lu@hotmail.com>
2022-02-15 16:40:05 -0500

    set up delete router in server side users.js and added test code to delete user with id 1 in client side

--- client/src/components/SideBar.js
+++ client/src/components/SideBar.js
@@ -16,8 +16,10 @@ import { createTheme, ThemeProvider, makeStyles } from '@material-ui/core/styles
 
 import axios from "axios";
 
+//sample template to Add/Edit tables in server, delete for production
+// `users/1` means to editing id = 1 from users table if id = 1 doesn't exist, system will add a new row with id = 1(create new)
+//possible table users, businesses, reports, services, categories, reward_points, ratings, 
 function testEditAxios() {
-  
 
 	return axios.put(`/users/1`, { 
 		first_name: 'Daniel',
@@ -31,6 +33,12 @@ function testEditAxios() {
       console.log("user1 edited", res)			
 		)
 }
+
+//sample template to Delete tables in server, delete for production
+// `users/1` means to delete the row with id = 1 in the users table 
+function testDeleteAxios() {
+	return axios.delete('/users/1')
+};
 
 const theme = createTheme({
 	palette: {
@@ -59,17 +67,17 @@ function SideBarStyled() {
 				}}>
 					<ListItemIcon>
 						<HomeIcon className='material-icons' />
-						<ListItemText primary="Home" />
+						<ListItemText primary="Home (testClick to ADD/Edit)" />
 					</ListItemIcon>
 				</ListItem>
 
 				<ListItem button divider className={classes.sideBarItem} onClick={() => {
-					alert('clicked');
+					testDeleteAxios();
 				}} >
 					
 					<ListItemIcon>
 						<span class="material-icons">public</span>
-						<ListItemText primary="Communities" />
+						<ListItemText primary="Communities (testClick to Delete)" />
 					</ListItemIcon>
 				</ListItem>
 
