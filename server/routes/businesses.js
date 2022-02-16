9c6525c9227265618f03ff48a209ddf72b44d47e
Daniel Lu <daniel_lu@hotmail.com>
2022-02-16 10:17:58 -0500

    route ADD/EDIT/DELETE created for all 7 tables and add proxy to client package.json

--- server/routes/businesses.js
+++ server/routes/businesses.js
@@ -2,23 +2,49 @@ const express = require('express');
 const router = express.Router();
 
 module.exports = (db) => {
-  // all routes will go here
+  // get a list of all businesses
   router.get('/', (req, res) => {
     const command = "SELECT * FROM businesses";
     db.query(command).then(data => {
       res.json(data.rows);
     });
   });
-
+  
+  // get value of a business by id
   router.get('/:id', (req, res) => {
     const command = `SELECT * FROM businesses
-    WHERE `
-    db.query(command).then(data => {
+    WHERE id = $1::integer`; 
+    const value = [req.params.id];
+    db.query(command, value).then(data => {
       res.json(data.rows);
     });
   });
 
+  // create/edit new business - ON CONFLICT in the query command below is used to determine the cases either create new or edit existed
+  router.put("/:id", (req, res) => {
+    
+    //req.body is axios put command's second parameter
+    console.log('req.body', req.body)
+    const { category_id, name, city, province_state, country, unit_number, street_address, postal_code, phone_number, website_url, image_url } = req.body;
 
-  
+    db.query(
+      `INSERT INTO businesses (category_id, name, city, province_state, country, unit_number, street_address, postal_code, phone_number, website_url, image_url, id)
+      VALUES ($1::integer, $2::text, $3::text, $4::text, $5::text, $6::text, $7::text, $8::text, $9::text, $10::text, $11::text, $12::integer)
+      ON CONFLICT (id) DO
+      UPDATE SET category_id = $1::integer, name = $2::text, city = $3::text, province_state = $4::text, country = $5::text, unit_number = $6::text, street_address = $7::text, postal_code = $8::text, phone_number = $9::text, website_url = $10::text, image_url = $11::text, modified_at = CURRENT_TIMESTAMP
+    `,
+      [category_id, name, city, province_state, country, unit_number, street_address, postal_code, phone_number, website_url, image_url, Number(req.params.id)]
+    )
+      .catch(error => console.log(error));
+  });
+
+  //delete a business
+  router.delete("/:id", (req, res) => {
+
+    db.query(`DELETE FROM businesses WHERE id = $1::integer`, [
+      req.params.id
+    ]);
+  });
+
   return router;
 };
--- server/routes/categories.js
+++ server/routes/categories.js
@@ -2,12 +2,47 @@ const express = require('express');
 const router = express.Router();
  
 module.exports = (db) => {
-  // all routes will go here
+  // get a list of all categories
   router.get('/', (req, res) => {
     const command = "SELECT * FROM categories";
     db.query(command).then(data => {
       res.json(data.rows);
     });
+  });
+
+  // get value of a category by id
+  router.get('/:id', (req, res) => {
+    const command = `SELECT * FROM categories
+    WHERE id = $1::integer`; 
+    const value = [req.params.id];
+    db.query(command, value).then(data => {
+      res.json(data.rows);
+    });
+  });
+
+  // create/edit new category - ON CONFLICT in the query command below is used to determine the cases either create new or edit existed
+  router.put("/:id", (req, res) => {
+    
+    //req.body is axios put command's second parameter
+    console.log('req.body', req.body)
+    const { name, image_url } = req.body;
+
+    db.query(
+      `INSERT INTO categories (name, image_url, id)
+      VALUES ($1::text, $2::text, $3::integer)
+      ON CONFLICT (id) DO
+      UPDATE SET name = $1::text, image_url = $2::text, modified_at = CURRENT_TIMESTAMP`,
+      [name, image_url, Number(req.params.id)]
+    )
+      .catch(error => console.log(error));
+  });
+
+  //delete a category
+  router.delete("/:id", (req, res) => {
+
+    db.query(`DELETE FROM categories WHERE id = $1::integer`, [
+      req.params.id
+    ]);
   });
 
   return router;
