2e19a01a5d070321f124a8188e68162cdbada800
Jaylen patterson <jaylenpatterson16@gmail.com>
2022-02-19 08:52:10 -0500

    display data from api to the front end

--- client/src/components/TopMembers.js
+++ client/src/components/TopMembers.js
@@ -10,40 +10,75 @@ import { CardHeader } from '@material-ui/core';
 import '../styles/TopMembers.css';
 
 function TopMembers() {
-	// const [ reportCount, setReportCount ] = useState('loading..');
-	// const [ userCount, setUserCount ] = useState('loading..');
+	// const [ userReportCount, setUserReportCount ] = useState('loading..');
+	const [ userCandyCount, setUserCandyCount ] = useState([]);
 	// // const [ displayReport, setDisplayReport ] = useState([]);
 
-	// useEffect(() => {
-	// 	getReportCount();
-	// 	getUserCount();
-	// }, []);
+	useEffect(() => {
+		getUserCandyCount();
+	}, []);
 
-	// const getReportCount = () => {
-	// 	return axios.get(`/reports/count_reports`).then((count) => {
-	// 		setReportCount(count.data[0].count);
-	// 	});
-	// };
+	const getUserCandyCount = () => {
+		return axios.get(`/reward_points/top_candy`).then((candyCount) => {
+			setUserCandyCount(candyCount.data);
+		});
+	};
 
-	// const getUserCount = () => {
-	// 	return axios.get(`/users/count_users`).then((count) => {
-	// 		setUserCount(count.data[0].count);
-	// 	});
-	// };
+	const memberList = userCandyCount.map((member) => {
+
+		return (
+			<CardHeader avatar={<Avatar>M</Avatar>} title={member.user} subheader={`172 Reports  ${member.candy} Candy`} />
+		)
+	})
 
 	return (
 		<Grid item lg={12} sm="none" xs={12} className="topMembers">
 			<Typography>
 				<span class="material-icons">leaderboard</span> Top Members
 			</Typography>
-
-			<CardHeader avatar={<Avatar>M</Avatar>} title="Michaela Palmer" subheader="172 Reports   52065 Candy" />
-
-			<CardHeader avatar={<Avatar>S</Avatar>} title="Samuel Brown" subheader="126 Reports   42656 Candy" />
-
-			<CardHeader avatar={<Avatar>L</Avatar>} title="Leon Task" subheader="89 Reports   25672 Candy" />
+			{memberList}
 		</Grid>
 	);
 }
 
 export default TopMembers;
+
+/*
+
+
+'
+SELECT user_name as user, reward_point as candy
+FROM users
+JOIN reward_points ON users.id = user_id
+ORDER BY reward_point
+DESC
+LIMIT 10;
+
+///
+
+SELECT reward_point, user_id 
+FROM reward_points
+JOIN users ON user_id = users.id
+ORDER BY reward_point
+DESC
+LIMIT 10;
+
+
+
+
+
+
+*/
+
+/*
+
+
+'SELECT * 
+FROM users
+JOIN reward_points ON users.id = user_id
+JOIN reports ON users.id = user_id`
+
+
+
+
+*/
